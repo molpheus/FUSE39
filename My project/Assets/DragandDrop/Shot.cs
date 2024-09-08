a94818023dbd086fecf641b923d64c0b3e3db88a
Daiki Kamata <zeal23300159@fsgcl.jp>
2024-09-08 16:27:37 +0900

    更新

--- My project/Assets/DragandDrop/Shot.cs
+++ My project/Assets/DragandDrop/Shot.cs
@@ -5,15 +5,18 @@ using UnityEngine;
 public class Shot : MonoBehaviour
 {
     public GameObject prePos;
+    public GameObject prant;
     private int EnergieAll;
     public EnergieAll Energie;
-    bool Push = false;
+    bool Push = false; 
+    bool Finish = false; 
+
     // Start is called before the first frame update
     // Update is called once per frame
     public void OnMouseDown()
     {
         //Energie = GetComponent<EnergieAll>();
-        EnergieAll = Energie.Energie1;
+        EnergieAll = Energie.Energie1 * 100;
         Push = true;
     }
 
@@ -23,9 +26,18 @@ public class Shot : MonoBehaviour
         {
             for(int i = 0; i < EnergieAll; i++)
             {
-                prePos.transform.position += new Vector3(0.0f,0.1f, 0.0f);
+                prant.transform.position -= new Vector3(0.0f,0.1f, 0.0f);
             }
+            Push = false;
+            Finish = true;
         }
-  
+        if(Finish== true && Push == false)
+        {
+            for(int i = 0;i < 10; i++)
+            {
+                prePos.transform.position -= new Vector3(0.0f, 0.01f, 0.0f);
+            }
+
+        } 
     }
 }
